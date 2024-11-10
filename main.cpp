ff9889673a98fb49b38d1a17d5d148b2879b644b
KILITY <114134521+KILITY@users.noreply.github.com>
2024-11-10 14:50:13 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main()
 {
-    ifstream inFile("input.txt");
+    vector<string> doctors, problems, problems_specialty;
+    ifstream inFile("/Users/killity/Vss projects/STL COURSE/STL/input.txt");
 
     int no_problems, no_doctors;
     string name, speciality;
-    
     inFile >> no_problems;
-
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        problems.push_back(name);
+        problems_specialty.push_back(speciality);
     }
 
     inFile >> no_doctors;
@@ -26,7 +26,21 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        doctors.push_back(speciality);
+    }
+
+    for (int i = 0; i < no_problems; i++)
+    {
+        int nr = 0;
+        for (int j = 0; j < no_doctors; j++)
+            if (problems_specialty[i] == doctors[j] && nr == 0)
+            {
+                cout << problems[i] << " acceptat"<<endl;
+                nr++;
+                break;
+            }
+        if (nr == 0)
+            cout<< problems[i] << " respins"<<endl;
     }
 
     return 0;
